6a1db3682ba338f3fa60d239887a6297c03b4961
Rio Capollari <rcapollari@oakland.edu>
2022-11-15 23:51:35 -0500

    adds some js

--- js/video.js
+++ js/video.js
@@ -1,1 +1,12 @@
 // Add js here.
+var video = document.getElementById("videoplayer")
+video.load();
+video.autoplay = false;
+video.loop = false;
+
+function playVid() {
+    video.play();
+}
+function pauseVid() {
+    video.pause();
+}
