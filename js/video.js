aca9264e968187b3c743da235bfa2d736d0487cf
Rio Capollari <rcapollari@oakland.edu>
2022-11-16 00:17:05 -0500

    implements more js functions

--- js/video.js
+++ js/video.js
@@ -10,3 +10,24 @@ function playVid() {
 function pauseVid() {
     video.pause();
 }
+function slowDown() {
+    video.playbackRate -= 0.5;
+    if (video.playbackRate == 0.5) {
+        alert("Video is at slowest speed!")
+    }
+}
+function speedUp() {
+    video.playbackRate += 0.5
+    if (video.playbackRate == 2) {
+        alert("Video is at fastest speed!")
+    }
+}
+function skipForward() {
+    video.currentTime += 15
+    if (video.ended == true) {
+        video.currentTime = 0
+    }
+}
+function muteVid() {
+    video.muted = true;
+}
